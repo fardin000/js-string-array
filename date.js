8ee16cd8776edea1ad52badea505f97f71a89d4d
fardin <abirfardin@gmail.com>
2021-09-20 04:49:59 +0600

    date issue discussed and try catch discussed

---
+++ date.js
@@ -0,0 +1,23 @@
+// const myFavDate = new Date('1971-12-16');
+// // console.log(myFavDate);
+// const anotherDate = new Date(1971, 3, 26, 11, 50, 40, 80);
+// // console.log(anotherDate);
+
+// ~
+
+// if(myFavDate.getTime < anotherDate){
+//     console.log('favourite is earlier');
+// }
+
+// //program ta run korle je dekhteche tar local time ta take dibe
+// //iccha kore onno kono desher time zone e convert korte pari (UTC) time function er maddhome ;
+
+
+
+// //date compare
+// //date gap 
+// //date vacation minus
+
+for(let i==0; i<10; i++){
+
+}
---
+++ try-catch.js
@@ -0,0 +1,29 @@
+// const name = 'ami';
+const myName = 'ami';
+// const student;
+const student = undefined;
+//can not read property
+// console.log(student.name);
+
+// for(let i = 0; i<10 i++){
+
+// }
+// name = 'tumi';
+// console.log(name);
+
+
+
+//////////////////////////////////////////////////
+//javascript error message search dao ;
+//onek jante parba ;
+//////////////////////////////////////////////////
+// throw ''
+
+try{
+    myName = 'tumi';
+}
+catch(error){
+   console.log('getting error', error);
+}
+console.log(myName);
+console.log(100);
