b643efd763efc4c7d1f4069b8bedc8a10dcff515
fardin <abirfardin@gmail.com>
2021-09-19 21:30:42 +0600

    case-converted

---
+++ case-convert.js
@@ -0,0 +1,13 @@
+const anthem = 'Aamar Sonar Bangla Ami Tomai Valobashi';
+const search = 'valo';
+
+//comparing string
+const userInput = 'blackPinK';
+const savedUserName = 'blackPink';
+// console.log(userInput);
+// console.log(userInput.toLocaleLowerCase());
+//console.log(savedUserName.toLocaleLowerCase());
+// if(userInput == savedUserName){
+    if(userInput.toLocaleLowerCase() == savedUserName.toLowerCase()){
+    console.log('user exists');
+}
