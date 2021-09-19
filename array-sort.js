3285720fb86bad02e0dcf821a949eb8f199b4d8c
fardin <abirfardin@gmail.com>
2021-09-20 02:25:45 +0600

    array-sort code added

---
+++ array-sort.js
@@ -0,0 +1,32 @@
+const numbers = [4, 7, 1, 3, 9, 2, 5];
+const sortNumbers = numbers.sort();
+// console.log(sortNumbers);
+
+
+// const friends = ['kabil', 'josim', 'razzak', 'anwar', 'deepjol','badsah'];
+// const sortFriends = friends.sort();
+// console.log(sortFriends);
+
+// const friends = ['kabil', 'josim', 'razzak', 'anwar', 'deepjol','badsah'];
+// const reversedFriends = friends.reverse();
+// console.log(reversedFriends);
+
+
+// const friends = ['kabil', 'josim', 'razzak', 'anwar', 'deepjol','badsah'];
+// const sortReversedFriends = friends.sort().reverse();
+// console.log(sortReversedFriends);
+//sorted thakbe kintu reverse way te 
+
+
+//number sorting fun
+//javascript sorting e problem kore
+const bigNumbers = [66, 58, 81, 92, 9, 6, 37, 13, 2];
+const sortedBigNumbers =bigNumbers.sort(function(a, b){
+    return a - b;
+})
+console.log(sortedBigNumbers);
+
+
+/*javascript single digit sort korte pare
+big numbers or digit sort korte problem kore, 
+tai compare function ante hoy */
